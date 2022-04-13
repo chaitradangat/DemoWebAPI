6c1e1c8342528cb28134c7f8e84c708b444e8335
Chaitra Dangat <chadangat@gmail.com>
2022-04-13 16:31:54 +0530

    mongo database service for CRUD

--- DemoWebAPI.DBService/MongoDbService.cs
+++ DemoWebAPI.DBService/MongoDbService.cs
@@ -30,5 +30,23 @@ namespace DemoWebAPI.DBService
             return _userDetails.Find(user => user.userid == id).FirstOrDefault();
         }
 
+        public UserDetail Create(UserDetail userDetail)
+        {
+            _userDetails.InsertOne(userDetail);
+
+            return userDetail;
+        }
+
+        public UserDetail Update(int id, UserDetail userDetail)
+        {
+            _userDetails.ReplaceOne(user => user.userid == id, userDetail);
+
+            return userDetail;
+        }
+
+        public void Remove(int id)
+        {
+            _userDetails.DeleteOne(user => user.userid == id);
+        }
     }
 }
