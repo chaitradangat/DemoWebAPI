c4900993c5631dfca185ebc70c52f3a3d4c9cf66
Chaitra Dangat <chadangat@gmail.com>
2022-04-13 15:45:58 +0530

    user service

---
+++ DemoWebAPI.UserService/IUserService.cs
@@ -0,0 +1,14 @@
+﻿using DemoWebAPI.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DemoWebAPI.UserService
+{
+    public interface IUserService
+    {
+        public List<UserDetail> Get();
+
+        public UserDetail Get(int id);
+    }
+}
---
+++ DemoWebAPI.UserService/UserService.cs
@@ -0,0 +1,29 @@
+﻿using DemoWebAPI.DBService;
+using DemoWebAPI.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DemoWebAPI.UserService
+{
+    public class UserService : IUserService
+    {
+        private readonly IDBService _dBService;
+
+        public UserService( IDBService dBService )
+        {
+            _dBService = dBService;
+        }
+
+        public List<UserDetail> Get()
+        {
+            return _dBService.Get();
+        }
+
+        public UserDetail Get(int id)
+        {
+            return _dBService.Get(id);
+        }
+
+    }
+}
