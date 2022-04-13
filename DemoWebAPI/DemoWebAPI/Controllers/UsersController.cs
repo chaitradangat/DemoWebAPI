84b05c3fbc1844415ad7f528c1863cbafeba87a5
Chaitra Dangat <chadangat@gmail.com>
2022-04-13 16:33:04 +0530

    db settings, DI mapping, mvc controller

---
+++ DemoWebAPI/DemoWebAPI/Controllers/UsersController.cs
@@ -0,0 +1,54 @@
+﻿using DemoWebAPI.Domain;
+using DemoWebAPI.UserService;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : Controller
+    {
+        private readonly IUserService _userService;
+
+        public UsersController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpGet]
+        public ActionResult<List<UserDetail>> Get()
+        {
+            return _userService.Get();
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<UserDetail> Get(int id)
+        {
+            return _userService.Get(id);
+        }
+
+        [HttpPost]
+        public ActionResult<UserDetail> Create(UserDetail userDetail)
+        {
+            return _userService.Create(userDetail);
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult<UserDetail> Update(int id, UserDetail userDetail)
+        {
+            return _userService.Update(id, userDetail);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Remove(int id)
+        {
+            _userService.Remove(id);
+
+            return NoContent();
+        }
+    }
+}
--- DemoWebAPI/DemoWebAPI/Startup.cs
+++ DemoWebAPI/DemoWebAPI/Startup.cs
@@ -1,3 +1,6 @@
+using DemoWebAPI.Domain;
+using DemoWebAPI.DBService;
+using DemoWebAPI.UserService;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -6,6 +9,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -25,6 +29,14 @@ namespace DemoWebAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<UsersDatabaseSettings>(Configuration.GetSection(nameof(UsersDatabaseSettings)));
+
+            services.AddSingleton<IUsersDatabaseSettings>(sp => sp.GetRequiredService<IOptions<UsersDatabaseSettings>>().Value);
+
+            services.AddSingleton<IDBService, MongoDbService>();
+
+            services.AddSingleton<IUserService, UserService.UserService>();
+
             services.AddControllers();
         }
 
